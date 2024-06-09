63b4dd0e7a1f748c031061b566c72be9712892a2
FADI BAKER <133148112+FadiBaker92@users.noreply.github.com>
2024-06-09 17:32:53 +0000

    created the dynamic dice route. added a rolls method to the pages_controller.rb and then rendered the html on the dice_rolls.html.erb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,16 @@ class PagesController < ApplicationController
     render({:template => "pages_template/home"})
   end
 
+  def rolls 
+    @num_dice = params.fetch("number_of_dice").to_i
+    @sides = params.fetch("how_many_sides").to_i
+    @rolls = []
+    @num_dice.times do 
+      dice = rand(1..@sides)
+      @rolls.push(dice)
+    end
+    render({:template => "pages_template/dice_rolls"})
+  end
+
 
 end
